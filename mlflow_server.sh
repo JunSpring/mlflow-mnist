34ee81d57f9c046e74a9c45878dbaa17b4437876
JunSpring <wjdwnsqja08@gmail.com>
2026-01-02 11:56:18 +0900

    Update

--- mlflow_server.sh
+++ mlflow_server.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
+PROJECT_ROOT="/home/junspring/mlflow-mnist"
+
 mlflow server \
-    --backend-store-uri sqlite:///mlflow.db \
-    --default-artifact-root ~/mlflow-mnist/mlartifacts \
+    --backend-store-uri "sqlite:///${PROJECT_ROOT}/mlflow.db" \
+    --default-artifact-root "${PROJECT_ROOT}/mlartifacts" \
     --host 0.0.0.0 \
     --port 5000
